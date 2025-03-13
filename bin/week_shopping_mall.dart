a617be3d0a7800ae5eaea936a2e1d88262d9bd90
ysshimx2 <ysshimx2@naver.com>
2025-03-13 17:28:14 +0900

    project1

---
+++ bin/week_shopping_mall.dart
@@ -0,0 +1,5 @@
+import 'package:week_shopping_mall/week_shopping_mall.dart' as week_shopping_mall;
+
+void main(List<String> arguments) {
+  print('Hello world: ${week_shopping_mall.calculate()}!');
+}
---
+++ test/week_shopping_mall_test.dart
@@ -0,0 +1,8 @@
+import 'package:week_shopping_mall/week_shopping_mall.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
