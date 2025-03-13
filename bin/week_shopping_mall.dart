eccce6cef05b14d1c8f88d5a10fe7d61ea1a2339
ysshimx2 <ysshimx2@naver.com>
2025-03-13 18:12:58 +0900

    base

--- bin/week_shopping_mall.dart
+++ bin/week_shopping_mall.dart
@@ -1,8 +1,44 @@
 import 'package:week_shopping_mall/week_shopping_mall.dart'
     as week_shopping_mall;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${week_shopping_mall.calculate()}!');
+class Product {
+  String name;
+  int price;
+
+  Product(this.name, this.price);
 }
 
-///dfgfgd
+class ShoppingMall {
+  List<Product> products = [];
+  int totalPrice = 0;
+
+  void addTocart(Product product) {
+    products.add(product);
+    totalPrice += product.price;
+  }
+
+  void removeFromCart(Product product) {
+    products.remove(product);
+    totalPrice -= product.price;
+  }
+
+  void showProducts() {
+    for (Product product in products) {
+      print(product.name);
+    }
+  }
+
+  void showTotalPrice() {
+    print(totalPrice);
+  }
+}
+
+void main(List<String> argument) {
+  while (true) {
+    var shoppingMall = ShoppingMall();
+
+    print("1 입력시 판매하고 있는 상품 목록 출력");
+    int input = int.parse(stdin.readLineSync()!);
+  }
+}
